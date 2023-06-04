3249743bc525f96546e58b7ac2f91f0465682ce5
Jorm <charkuayteow.10@gmail.com>
2023-06-04 16:20:11 +0800

    quick fix because i fcked up

--- src/components/Square.js
+++ src/components/Square.js
@@ -6,7 +6,7 @@ const Square = ({ colorValue, hexValue, isDarkText }) => {
       className="rounded-xl grid w-96 h-96 border-2 border-none shadow-2xl place-content-center bg-slate-200"
       style={{
         backgroundColor: colorValue,
-        color: isDarkText ? "#f8fafc" : "#94a3b8",
+        color: isDarkText ? "#94a3b8" : "#f8fafc",
       }}
     >
       <h3>{colorValue ? colorValue : "start typing"}</h3>
